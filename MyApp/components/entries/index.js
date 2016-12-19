49a65c817505bce28896ad1928a18f2a87c2f9bd
garshen williams <garshenw@gmail.com>
2016-12-19 12:50:52 +0000

    Changes made by garshen williams (garshenw@gmail.com). adding entries

---
+++ MyApp/components/entries/index.js
@@ -0,0 +1,424 @@
+'use strict';
+
+app.entries = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('entries');
+
+// START_CUSTOM_CODE_entries
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_entries
+(function(parent) {
+    var dataProvider = app.data.myApp,
+        /// start global model properties
+        /// end global model properties
+        fetchFilteredData = function(paramFilter, searchFilter) {
+            var model = parent.get('entriesModel'),
+                dataSource;
+
+            if (model) {
+                dataSource = model.get('dataSource');
+            } else {
+                parent.set('entriesModel_delayedFetch', paramFilter || null);
+                return;
+            }
+
+            if (paramFilter) {
+                model.set('paramFilter', paramFilter);
+            } else {
+                model.set('paramFilter', undefined);
+            }
+
+            if (paramFilter && searchFilter) {
+                dataSource.filter({
+                    logic: 'and',
+                    filters: [paramFilter, searchFilter]
+                });
+            } else if (paramFilter || searchFilter) {
+                dataSource.filter(paramFilter || searchFilter);
+            } else {
+                dataSource.filter({});
+            }
+        },
+
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Entries',
+                dataProvider: dataProvider
+            },
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    /// start flattenLocation property
+                    flattenLocationProperties(dataItem);
+                    /// end flattenLocation property
+
+                }
+            },
+            error: function(e) {
+
+                if (e.xhr) {
+                    var errorText = "";
+                    try {
+                        errorText = JSON.stringify(e.xhr);
+                    } catch (jsonErr) {
+                        errorText = e.xhr.responseText || e.xhr.statusText || 'An error has occurred!';
+                    }
+                    alert(errorText);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Entry': {
+                            field: 'Entry',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+            serverFiltering: true,
+            serverSorting: true,
+            sort: {
+                field: 'Entry',
+                dir: 'asc'
+            },
+            serverPaging: true,
+            pageSize: 50
+        },
+        /// start data sources
+        /// end data sources
+        entriesModel = kendo.observable({
+            _dataSourceOptions: dataSourceOptions,
+            searchChange: function(e) {
+                var searchVal = e.target.value,
+                    searchFilter;
+
+                if (searchVal) {
+                    searchFilter = {
+                        field: 'Entry',
+                        operator: 'contains',
+                        value: searchVal
+                    };
+                }
+                fetchFilteredData(entriesModel.get('paramFilter'), searchFilter);
+            },
+            fixHierarchicalData: function(data) {
+                var result = {},
+                    layout = {};
+
+                $.extend(true, result, data);
+
+                (function removeNulls(obj) {
+                    var i, name,
+                        names = Object.getOwnPropertyNames(obj);
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+
+                        if (obj[name] === null) {
+                            delete obj[name];
+                        } else if ($.type(obj[name]) === 'object') {
+                            removeNulls(obj[name]);
+                        }
+                    }
+                })(result);
+
+                (function fix(source, layout) {
+                    var i, j, name, srcObj, ltObj, type,
+                        names = Object.getOwnPropertyNames(layout);
+
+                    if ($.type(source) !== 'object') {
+                        return;
+                    }
+
+                    for (i = 0; i < names.length; i++) {
+                        name = names[i];
+                        srcObj = source[name];
+                        ltObj = layout[name];
+                        type = $.type(srcObj);
+
+                        if (type === 'undefined' || type === 'null') {
+                            source[name] = ltObj;
+                        } else {
+                            if (srcObj.length > 0) {
+                                for (j = 0; j < srcObj.length; j++) {
+                                    fix(srcObj[j], ltObj[0]);
+                                }
+                            } else {
+                                fix(srcObj, ltObj);
+                            }
+                        }
+                    }
+                })(result, layout);
+
+                return result;
+            },
+            itemClick: function(e) {
+                var dataItem = e.dataItem || entriesModel.originalItem;
+
+                app.mobileApp.navigate('#components/entries/details.html?uid=' + dataItem.uid);
+
+            },
+            addClick: function() {
+                app.mobileApp.navigate('#components/entries/add.html');
+            },
+            editClick: function() {
+                var uid = this.originalItem.uid;
+                app.mobileApp.navigate('#components/entries/edit.html?uid=' + uid);
+            },
+            deleteItem: function() {
+                var dataSource = entriesModel.get('dataSource');
+
+                dataSource.remove(this.originalItem);
+
+                dataSource.one('sync', function() {
+                    app.mobileApp.navigate('#:back');
+                });
+
+                dataSource.one('error', function() {
+                    dataSource.cancelChanges();
+                });
+
+                dataSource.sync();
+            },
+            deleteClick: function() {
+                var that = this;
+
+                navigator.notification.confirm(
+                    'Are you sure you want to delete this item?',
+                    function(index) {
+                        //'OK' is index 1
+                        //'Cancel' - index 2
+                        if (index === 1) {
+                            that.deleteItem();
+                        }
+                    },
+                    '', ['OK', 'Cancel']
+                );
+            },
+            detailsShow: function(e) {
+                var uid = e.view.params.uid,
+                    dataSource = entriesModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(uid);
+
+                entriesModel.setCurrentItemByUid(uid);
+
+                /// start detail form show
+                /// end detail form show
+            },
+            setCurrentItemByUid: function(uid) {
+                var item = uid,
+                    dataSource = entriesModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+
+                if (!itemModel.Entry) {
+                    itemModel.Entry = String.fromCharCode(160);
+                }
+
+                /// start detail form initialization
+                /// end detail form initialization
+
+                entriesModel.set('originalItem', itemModel);
+                entriesModel.set('currentItem',
+                    entriesModel.fixHierarchicalData(itemModel));
+
+                return itemModel;
+            },
+            linkBind: function(linkString) {
+                var linkChunks = linkString.split('|');
+                if (linkChunks[0].length === 0) {
+                    return this.get('currentItem.' + linkChunks[1]);
+                }
+                return linkChunks[0] + this.get('currentItem.' + linkChunks[1]);
+            },
+            /// start masterDetails view model functions
+            /// end masterDetails view model functions
+            currentItem: {}
+        });
+
+    parent.set('addItemViewModel', kendo.observable({
+        /// start add model properties
+        /// end add model properties
+        /// start add model functions
+        /// end add model functions
+        onShow: function(e) {
+            this.set('addFormData', {
+                textAreaField: '',
+                date6: '',
+                /// start add form data init
+                /// end add form data init
+            });
+            /// start add form show
+            /// end add form show
+        },
+        onCancel: function() {
+            /// start add model cancel
+            /// end add model cancel
+        },
+        onSaveClick: function(e) {
+            var addFormData = this.get('addFormData'),
+                filter = entriesModel && entriesModel.get('paramFilter'),
+                dataSource = entriesModel.get('dataSource'),
+                addModel = {};
+
+            function saveModel(data) {
+                /// start add form data save
+                addModel.Entry = addFormData.textAreaField;
+                addModel.Date = addFormData.date6;
+                /// end add form data save
+
+                dataSource.add(addModel);
+                dataSource.one('change', function(e) {
+                    app.mobileApp.navigate('#:back');
+                });
+
+                dataSource.sync();
+                app.clearFormDomData('add-item-view');
+            };
+
+            /// start add form save
+            /// end add form save
+            /// start add form save handler
+            saveModel();
+            /// end add form save handler
+        }
+    }));
+
+    parent.set('editItemViewModel', kendo.observable({
+        /// start edit model properties
+        /// end edit model properties
+        /// start edit model functions
+        /// end edit model functions
+        editFormData: {},
+        onShow: function(e) {
+            var that = this,
+                itemUid = e.view.params.uid,
+                dataSource = entriesModel.get('dataSource'),
+                itemData = dataSource.getByUid(itemUid),
+                fixedData = entriesModel.fixHierarchicalData(itemData);
+
+            /// start edit form before itemData
+            /// end edit form before itemData
+
+            this.set('itemData', itemData);
+            this.set('editFormData', {
+                textAreaField1: itemData.Entry,
+                date7: itemData.Date,
+                /// start edit form data init
+                /// end edit form data init
+            });
+
+            /// start edit form show
+            /// end edit form show
+        },
+        linkBind: function(linkString) {
+            var linkChunks = linkString.split(':');
+            return linkChunks[0] + ':' + this.get('itemData.' + linkChunks[1]);
+        },
+        onSaveClick: function(e) {
+            var that = this,
+                editFormData = this.get('editFormData'),
+                itemData = this.get('itemData'),
+                dataSource = entriesModel.get('dataSource');
+
+            /// edit properties
+            itemData.set('Entry', editFormData.textAreaField1);
+            itemData.set('Date', editFormData.date7);
+            /// start edit form data save
+            /// end edit form data save
+
+            function editModel(data) {
+                /// start edit form data prepare
+                /// end edit form data prepare
+                dataSource.one('sync', function(e) {
+                    /// start edit form data save success
+                    /// end edit form data save success
+
+                    app.mobileApp.navigate('#:back');
+                });
+
+                dataSource.one('error', function() {
+                    dataSource.cancelChanges(itemData);
+                });
+
+                dataSource.sync();
+                app.clearFormDomData('edit-item-view');
+            };
+            /// start edit form save
+            /// end edit form save
+            /// start edit form save handler
+            editModel();
+            /// end edit form save handler
+        },
+        onCancel: function() {
+            /// start edit form cancel
+            /// end edit form cancel
+        }
+    }));
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('entriesModel', entriesModel);
+            var param = parent.get('entriesModel_delayedFetch');
+            if (typeof param !== 'undefined') {
+                parent.set('entriesModel_delayedFetch', undefined);
+                fetchFilteredData(param);
+            }
+        });
+    } else {
+        parent.set('entriesModel', entriesModel);
+    }
+
+    parent.set('onShow', function(e) {
+        var param = e.view.params.filter ? JSON.parse(e.view.params.filter) : null,
+            isListmenu = false,
+            backbutton = e.view.element && e.view.element.find('header [data-role="navbar"] .backButtonWrapper'),
+            dataSourceOptions = entriesModel.get('_dataSourceOptions'),
+            dataSource;
+
+        if (param || isListmenu) {
+            backbutton.show();
+            backbutton.css('visibility', 'visible');
+        } else {
+            if (e.view.element.find('header [data-role="navbar"] [data-role="button"]').length) {
+                backbutton.hide();
+            } else {
+                backbutton.css('visibility', 'hidden');
+            }
+        }
+
+        dataSource = new kendo.data.DataSource(dataSourceOptions);
+        entriesModel.set('dataSource', dataSource);
+        fetchFilteredData(param);
+    });
+
+})(app.entries);
+
+// START_CUSTOM_CODE_entriesModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_entriesModel
--- MyApp/components/formView/index.js
+++ MyApp/components/formView/index.js
@@ -14,68 +14,13 @@ app.localization.registerView('formView');
     var
     /// start global model properties
     /// end global model properties
-        dataProvider = app.data.myApp,
-        dataSourceOptions = {
-            type: 'everlive',
-            transport: {
-                typeName: 'Entries',
-                dataProvider: dataProvider
-            },
-            error: function(e) {
-                if (e.xhr) {
-                    alert(JSON.stringify(e.xhr));
-                }
-            }
-        },
         formViewModel = kendo.observable({
-            submit: function() {
-                var addFormData = parent.get('addFormData'),
-                    addModel = {};
+        submit: function() {},
+        /// start add model functions
+        /// end add model functions
 
-                app.mobileApp.showLoading();
-
-                function saveModel(data) {
-                    /// start add form data save
-                    addModel.Entry = addFormData.textAreaField;
-                    addModel.Date = addFormData.date6;
-                    /// end add form data save
-                    var dataSource = new kendo.data.DataSource(dataSourceOptions);
-                    dataSource.add(addModel);
-                    dataSource.one('change', function(e) {
-                        // datasource operation finished
-                        app.mobileApp.hideLoading();
-                        app.showNotification('Saved');
-                    });
-
-                    dataSource.one('error', function(error) {
-                        showErrorMessage(error.xhr || error);
-                    });
-
-                    dataSource.sync();
-                };
-
-                function showErrorMessage(error) {
-                    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-                    app.mobileApp.hideLoading();
-                }
-
-                /// start add form save
-                /// end add form save
-                /// start add form save handler
-                saveModel();
-                /// end add form save handler
-            },
-            /// start add model functions
-            /// end add model functions
-
-            cancel: function() {
-                    /// start add model cancel
-                    /// end add model cancel
-                }
-                /// start add model properties
-                /// end add model properties
-
-        });
+        cancel: function() {}
+    });
 
     /// start form functions
     /// end form functions
@@ -83,8 +28,6 @@ app.localization.registerView('formView');
     parent.set('onShow', function _onShow() {
         var that = parent;
         that.set('addFormData', {
-            textAreaField: '',
-            date6: '',
             /// start add form data init
             /// end add form data init
         });
